c0a019def8765f3935d9ad8a2a47cff47bdd6ef1
Tepex <tepex@mail.ru>
2026-02-18 11:52:06 +0300

    fix(fts): add `BuilderName` annotation. Fix comments.

--- fts/src/main/kotlin/Entity.kt
+++ fts/src/main/kotlin/Entity.kt
@@ -2,5 +2,5 @@ package ru.it_arch.kddd.fts
 
 public interface Entity : Fts, Validatable {
     public val id: ValueObject
-    public val content: ValueObject.Data
+    public val content: ValueObject
 }
